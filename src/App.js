33552d90ddc8d6b690bf831f44fca0a5d58bf479
steve27519 <stevenyurchuck@gmail.com>
2018-07-15 14:26:49 -0400

    imgscss

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import Navbar from "./components/Navbar";
-import Header from "./components/Header";
+import Navbar from "./components/navbar.js";
+import Header from "./components/header.js";
 import Main from "./components/Main";
-import Image from "./components/Image";
+import Image from "./components/image.js";
 import Img from "./components/Img.json";
-//image imports 
-// import bart from "./images/bart.jpg";
+//image imports
+
 import './App.css';
 
 class App extends Component {
@@ -13,13 +13,12 @@ class App extends Component {
     picked: [],
     correct: 0,
     topscore: 0,
-    message: "Click an image to begin"
+    message: 'Click an image to begin'
   };
 
   // random shuffle
   shuffleArray = (array) => {
     let imgArray = Img;
-    console.log("img 4", Img[3]);
     for (let i = imgArray.length - 1; i > 0; i--) {
       let j = Math.floor(Math.random() * (i + 1));
       [imgArray[i], imgArray[j]] = [imgArray[j], imgArray[i]];
@@ -27,16 +26,17 @@ class App extends Component {
     return imgArray
   }
 
-  pickImg = (name) => {
+  pickImg = (id) => {
     console.log("Clicked");
     let picked = this.state.picked;
-    
-    if (picked.indexOf(name) === -1) {
+    console.log("picked", picked);
+    if (picked.indexOf(id) === -1) {
+      console.log("in -1 name", id);
       this.setState({
-        picked: picked.concat(name),
+        picked: picked.concat(id),
         correct: this.state.correct + 1,
         topscore: this.state.correct + 1 > this.state.topscore ? this.state.correct + 1 : this.state.topscore,
-        message: "Excellent!" 
+        message: "Excellent!"
       })
       this.shuffleArray();
     }
@@ -49,54 +49,22 @@ class App extends Component {
     }
   }
 
-  // imgSwitch = (name) => {
-  //   switch (name) {
-  //     case "apu":
-  //       return "${apu}"
-  //     case "bart":
-  //       return "${bart}"
-  //     case "beavis":
-  //       return "${beavis}"
-  //     case "hank":
-  //       return "${hank}"
-  //     case "krusty":
-  //       return "${krusty}"
-  //     case "lisa":
-  //       return "${lisa}"
-  //     case "lois":
-  //       return "${lois}"
-  //     case "mrburns":
-  //       return "${mrburns}"
-  //     case "ned":
-  //       return "${ned}"
-  //     case "peggy":
-  //       return "${peggy}"
-  //     case "peter":
-  //       return "${peter}"
-  //     case "butthead":
-  //       return "${butthead}"
-  //     default:
-  //       return "${apu}"
-  //   }
-  // }
-
   render() {
     return (
       <div>
-        
+
         <Navbar correct={this.state.correct} topscore={this.state.topscore} message={this.state.message}/>
         <Header />
         <Main>
-          
+
             {this.shuffleArray(Img).map(image => (
-            
-            <Image key={image.id} url={image.image} name={image.name} pickImg={this.pickImg}  />
+
+            <Image key={image.id} id={image.id} url={image.image} pickImg={this.pickImg}  />
           ))}
         </Main>
-        
+
               </div>
     );
   }
 }
-
 export default App;
--- src/components/image.js
+++ src/components/image.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
-const Image = (props) => 
-<div>
-  <p> {props.url}</p>
-  <img src={props.url} alt={props.name} className="img-thumbnail clickable-images" onClick={() => props.pickImg(props.name)} />
-</div>
+const Image = (props) =>
+  <div className="characterDiv">
+    <p> {props.url}</p>
+    <img src={props.url} className="img-thumbnail clickable-images" onClick={() => props.pickImg(props.id)} />
+  </div>
+
 
 export default Image;
