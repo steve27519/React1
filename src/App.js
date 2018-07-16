fa4f26afc4e9916734f164f8496a3ea1273c62f9
steve27519 <stevenyurchuck@gmail.com>
2018-07-15 20:36:36 -0400

    images

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import Navbar from "./components/navbar.js";
-import Header from "./components/header.js";
+import Navbar from "./components/Navbar.js";
+import Header from "./components/Header.js";
 import Main from "./components/Main";
-import Image from "./components/image.js";
+import Image from "./components/Image.js";
 import Img from "./components/Img.json";
 //image imports
 
--- src/components/image.js
+++ src/components/image.js
@@ -1,10 +1,5 @@
-import React from 'react';
-
 const Image = (props) =>
   <div className="characterDiv">
     <p> {props.url}</p>
-    <img src={props.url} className="img-thumbnail clickable-images" onClick={() => props.pickImg(props.id)} />
+    <img src={props.url} alt="character" className="img-thumbnail clickable-images" onClick={() => props.pickImg(props.id)} />
   </div>
-
-
-export default Image;
