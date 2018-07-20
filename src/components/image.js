b4d48c2e81daad930df4773c191690a059dcf996
steve27519 <stevenyurchuck@gmail.com>
2018-07-19 21:17:28 -0400

    working

--- src/components/image.js
+++ src/components/image.js
@@ -1,5 +1,9 @@
+import React from 'react';
+
 const Image = (props) =>
   <div className="characterDiv">
     <p> {props.url}</p>
     <img src={props.url} alt="character" className="img-thumbnail clickable-images" onClick={() => props.pickImg(props.id)} />
   </div>
+
+  export default Image;
